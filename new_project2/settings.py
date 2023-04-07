ecb9d44aa17d5c4279868055e5f6c9eab304dcc6
Arya Shah <arya.shah82@nmims.edu.in>
2023-04-07 15:09:27 +0530

    edit files

--- new_project2/settings.py
+++ new_project2/settings.py
@@ -32,11 +32,11 @@ ALLOWED_HOSTS = ['.vercel.app']
 
 INSTALLED_APPS = [
     # 'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    # 'django.contrib.auth',
+    # 'django.contrib.contenttypes',
+    # 'django.contrib.sessions',
+    # 'django.contrib.messages',
+    # 'django.contrib.staticfiles',
     'finalapp'
 ]
 
