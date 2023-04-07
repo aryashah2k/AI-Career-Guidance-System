fc9a01f6b661403aa84c37727dbca0167752da3e
Arya Shah <arya.shah82@nmims.edu.in>
2023-04-07 15:16:17 +0530

    revert vercel

--- new_project2/settings.py
+++ new_project2/settings.py
@@ -25,18 +25,18 @@ SECRET_KEY = 'django-insecure-s613gplk2qtj@#8p$4c%fil6w^=9^y$^_frnt5=$kqfr*!p(3i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    # 'django.contrib.admin',
-    # 'django.contrib.auth',
-    # 'django.contrib.contenttypes',
-    # 'django.contrib.sessions',
-    # 'django.contrib.messages',
-    # 'django.contrib.staticfiles',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
     'finalapp'
 ]
 
--- new_project2/wsgi.py
+++ new_project2/wsgi.py
@@ -15,4 +15,4 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'new_project2.settings')
 
 application = get_wsgi_application()
 
-app = application
+# app = application
